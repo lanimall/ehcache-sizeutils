38649d9d9f2a911dffc707dd424c548223ef6fd2
Fabien Sanglier <fabiensanglier@gmail.com>
2014-03-12 00:12:57 -0400

    extra capabilities

---
+++ scripts/setenv.sh
@@ -0,0 +1,9 @@
+#this is the classpath of the client libraries (with business objects used in cache)
+CLIENT_CLASSPATH=$HOME/MyDev/MyTools/JPerfTester/TerracottaEhCacheTesterSuite/TesterClients/GenericCacheTester/target/jperftester-terracotta-ehcache-client-genericcachetester-0.0.1.jar
+
+#client path for ehcache.xml (either file path using file:// or classpath path using classpath://) - [optional if ehcache.xml already in above CLIENT_CLASSPATH]
+EHCACHE_PATH=
+
+#license path
+TC_PRODUCTKEY=$HOME/terracotta-license.key
+
